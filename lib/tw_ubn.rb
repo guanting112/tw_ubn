1c0a7f1c9c11659d579e5742c0a42377fae36ee6
Guanting Chen ( @Elf ) <mym8899gt@gmail.com>
2021-12-12 03:40:48 +0800

    create repo

---
+++ lib/tw_ubn.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tw_ubn/version"
+
+module TwUbn
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tw_ubn/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TwUbn
+  VERSION = "0.1.0"
+end
